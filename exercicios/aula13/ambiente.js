d4458dda047cd10893ef2251668e45d7024d39cc
luccalopes <110003708+luccalopes@users.noreply.github.com>
2022-08-30 10:11:49 -0300

    exercicio

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,31 @@
+var c = 1
+/*do {
+    console.log(`Passo ${c}`)
+    c++
+} while(c <= 10)
+*/
+
+
+
+
+/*var c = 1
+while(c <= 6) {
+    console.log('tudo bem?')
+    c++
+}
+*/
+
+
+
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
+
+for(var c = 1;c <= 5;c++) {
+    console.log(c)
+}
