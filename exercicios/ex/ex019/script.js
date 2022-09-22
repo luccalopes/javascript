420540010aaf4a2ecd77226809f3dd1181761dd6
luccalopes <110003708+luccalopes@users.noreply.github.com>
2022-09-22 15:32:42 -0300

    att

--- exercicios/ex/ex019/script.js
+++ exercicios/ex/ex019/script.js
@@ -2,6 +2,9 @@ function fatorial() {
     let saida = document.getElementById('saida')
     let n = Number(document.getElementById('txtn').value)
 
+    if (n >= 22) {
+        window.alert('Por favor, escolha um número menor.')
+    } else {
     saida.innerHTML += `<h2>Calculando ${n}</h2>`
     let c = n
     let fat = 1
@@ -10,5 +13,11 @@ function fatorial() {
         fat *= c
         c--
     }
-    saida.innerHTML += `1 = <strong>${fat}</strong>`
+    saida.innerHTML += `1 = <strong>${fat.toLocaleString('pt-BR')}</strong>`
+    }
+}
+
+function limpar() {
+    let saida = document.getElementById('saida')
+    saida.innerHTML = `Preencha os dados`
 }
