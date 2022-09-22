92df2fe3d7e8ed7935fa3b1a551f1348307aa4f6
luccalopes <110003708+luccalopes@users.noreply.github.com>
2022-09-21 21:37:22 -0300

    att

--- exercicios/ex/ex018/script.js
+++ exercicios/ex/ex018/script.js
@@ -1,18 +1,64 @@
 function tabuada() {
-    let num = document.getElementById('txtn')
     let tab = document.getElementById('seltab')
+    let num = document.getElementById('txtn')
 
-    if (num.value.lenght == 0) {
-        window.alert('Por favor digite um número')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número.')
     } else {
         let n = Number(num.value)
         let c = 1
         tab.innerHTML = ''
-        while (c <= 10) {
+
+        for (let c = 1; c <= 10; c ++) {
             let item = document.createElement('option')
-            item.text = `${n} x ${c} = ${(n * c)}`
+            item.text = `${n} x ${c} = ${n * c}`
+            item.value = `${c}`
             tab.appendChild(item)
-            c++
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function tabuada() {
+    let tab = document.getElementById('seltab')
+    let num = document.getElementById('txtn')
+
+    if (num.value.lenght == 0) {
+        window.alert('Por favor, insira um número')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+
+        for (c = 1; c <= 10; c ++) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n * c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+    }
+}*/
