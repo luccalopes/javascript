270e8aeeda7044e7080b1056f66ae80722c4ddb9
luccalopes <110003708+luccalopes@users.noreply.github.com>
2022-09-23 22:13:58 -0300

    att

---
+++ exercicios/ex/ex023/script.js
@@ -0,0 +1,9 @@
+let dol = Number(window.prompt('Qual a atual cotação do dólar?'))
+
+function converter() {
+    let res = document.getElementById('res')
+    let cotacaoAtual = dol.toLocaleString('pt-BR', {style:'currency', currency: 'BRL'})
+    let brl = Number(window.prompt('Quanto você tem em dinheiro?'))
+    let numeroDolar = 
+
+}
