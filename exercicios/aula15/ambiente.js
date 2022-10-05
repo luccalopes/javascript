e1064595e299c5559b2406e5c5fcd1d52e3300d5
luccalopes <110003708+luccalopes@users.noreply.github.com>
2022-10-05 10:29:06 -0300

    att

--- exercicios/aula15/ambiente.js
+++ exercicios/aula15/ambiente.js
@@ -1,2 +1,6 @@
 var num = [3, 5, 7, 9]
+num.push(1)
+num.sort()
 console.log(`Nosso vetor é ${num}`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+console.log(`O vetor tem ${num.length} posições.`)
